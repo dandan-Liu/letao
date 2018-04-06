dfed1905b36a9607a753bc03364fa40108d175f6
dandan-Liu <123@qq.com>
2018-04-06 21:55:46 +0800

    首页基本功能提交

--- public/back/js/common.js
+++ public/back/js/common.js
@@ -9,3 +9,36 @@ $(document).ajaxStop(function(){
     NProgress.done();
   },500);
 });
+
+$(function(){
+  //二级菜单下拉功能
+  $(".category").on("click",function(){
+    $(this).next().stop().slideToggle();
+  });
+
+  //顶部菜单栏切换功能
+  $(".icon_menu").on("click",function(){
+    $(".lt_aside").toggleClass("hidemenu");
+    $(".lt_main").toggleClass("hidemenu");
+    $(".lt_topbar").toggleClass("hidemenu");
+  });
+
+  //模态框
+  $(".icon_logout").on("click",function(){
+    $("#logoutModal").modal("show");
+  });
+
+  $("#logoutBtn").on("click",function(){
+    $.ajax({
+      type:"get",
+      url:"/employee/employeeLogout",
+      dataType:"json",
+      success:function(info){
+        //console.log(info);
+        if(info.success){
+          location.href="login.html";
+        }
+      }
+    });
+  });
+});
